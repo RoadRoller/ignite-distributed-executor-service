97e88de57c979b1e23262f40e033537efeccd383
Evgeny Tarasenko <tarasenko.evgeny@gmail.com>
2018-12-04 20:37:11 +0700

    Init emptry Spring Boot application

---
+++ src/main/java/org/nprog/demo/ignite/IgniteApplication.java
@@ -0,0 +1,12 @@
+package org.nprog.demo.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteApplication.class, args);
+	}
+}
